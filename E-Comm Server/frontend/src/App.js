35a6b6a46cff6cd90c4e0635d8cd616497d1f57b
jspmern <jspmern@gmail.com>
2023-09-27 15:51:59 +0530

    private routing

--- E-Comm Server/frontend/src/App.js
+++ E-Comm Server/frontend/src/App.js
@@ -7,7 +7,10 @@ import Footer from './components/Footer'
 import Home from './components/pages/Home'
 import Login from './components/auth/Login'
 import Signup from './components/auth/Signup'
-// import Login from './components/auth/Login'
+import Products from './components/pages/Products'
+import About from './components/pages/About'
+import Contact from './components/pages/Contact'
+import PrivateRoute from './components/private/PrivateRoute'
 
 function App() {
   return (
@@ -16,6 +19,11 @@ function App() {
         <Nav></Nav>
         <Routes>
           <Route path='/' element={<Home></Home>}></Route>
+          <Route element={<PrivateRoute></PrivateRoute>}>
+            <Route path='/products' element={<Products />}></Route>
+            <Route path='/about' element={<About />}></Route>
+            <Route path='/contact' element={<Contact></Contact>}></Route>
+          </Route>
           <Route path='/login' element={<Login></Login>}></Route>
           <Route path='/signup' element={<Signup></Signup>}></Route>
         </Routes>
